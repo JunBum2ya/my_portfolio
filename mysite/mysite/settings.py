3b8176ceeec5e611405cf878a3302dc0b8d9578d
leejunbum <22cun2@naver.com>
2020-11-08 16:23:02 +0900

    secret

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,7 +32,7 @@ EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
 EMAIL_HOST_USER = '22cun2@gmail.com'
-EMAIL_HOST_PASSWORD='ljb659496$'
+EMAIL_HOST_PASSWORD='password'
 
 
 # Application definition
